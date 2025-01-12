a647ed2f19ed3b77e2048bb144ec718d92a01d2c
gurjot74 <gagan.singh@tothenew.com>
2025-01-13 02:50:40 +0530

    aws-deploy

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const servers = ["server1", "server2"];
 let ragiList = JSON.parse(fs.readFileSync("./ragiList.json", "UTF-8"));
 const delayByRagis = 210000;
 
-setInterval(function () {
-  //for preventing free deployed server to become idle
-  https.get(process.env.deployedUrl);
-}, 500000);
+// setInterval(function () {
+//   //for preventing free deployed server to become idle
+//   https.get(process.env.deployedUrl);
+// }, 500000);
 
 const ragiListUpdateScheduler = async () => {
   try {
@@ -34,14 +34,14 @@ const ragiListUpdateScheduler = async () => {
 };
 
 const recordStream = async (duty, endMilliseconds, to, from) => {
-  // if (!((await redisClient.get("currentServer")) === process.env.currentServer))
-  //   return;
-  // setTimeout(async () => {
-  //   await redisClient.set(
-  //     "currentServer",
-  //     servers.find((server) => server !== process.env.currentServer),
-  //   );
-  // }, 59000);
+  if (!((await redisClient.get("currentServer")) === process.env.currentServer))
+    return;
+  setTimeout(async () => {
+    await redisClient.set(
+      "currentServer",
+      servers.find((server) => server !== process.env.currentServer),
+    );
+  }, 59000);
   console.log("recordinds ends after ", endMilliseconds, "milliseconds");
   const liveStreamSgpcUrl = "https://live.sgpc.net:8443/;nocache=889869";
   var currentIndianDate = getIndianDate();
